ecd0735562b82415a8a9ea2723f7504509e959ca
aitoshuu <619492003@qq.com>
2025-12-11 22:38:42 +0800

    ratio

--- components/Game.tsx
+++ components/Game.tsx
@@ -16,7 +16,7 @@ import { TransitionContext } from '../App';
  * - Use postMessage to communicate pause/resume with Unity
  * - Gray overlay with blur effect when paused
  */
-const GAME_URL = '/game/index.html?v=20250223';
+const GAME_URL = '/game/index.html?v=20250224';
 
 const Game: React.FC = () => {
   // Get text visibility state from App.tsx's TransitionContext
